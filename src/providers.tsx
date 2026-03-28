cc2fdf743cb81778f5a447c79cb164a1dece9e7f
r0hansaxena <rohansxn8772@gmail.com>
2026-03-28 16:08:27 +0530

    setup wagmi and rainbowkit providers

---
+++ src/providers.tsx
@@ -0,0 +1,29 @@
+"use client";
+
+import "@rainbow-me/rainbowkit/styles.css";
+
+import { RainbowKitProvider, darkTheme } from "@rainbow-me/rainbowkit";
+import { WagmiProvider } from "wagmi";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { config } from "@/config";
+import type { ReactNode } from "react";
+
+const queryClient = new QueryClient();
+
+export default function Providers({ children }: { children: ReactNode }) {
+  return (
+    <WagmiProvider config={config}>
+      <QueryClientProvider client={queryClient}>
+        <RainbowKitProvider
+          theme={darkTheme({
+            accentColor: "#836EF9",
+            accentColorForeground: "white",
+            borderRadius: "medium",
+          })}
+        >
+          {children}
+        </RainbowKitProvider>
+      </QueryClientProvider>
+    </WagmiProvider>
+  );
+}
